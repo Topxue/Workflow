dd6ba4438f220fdad2b3088f1358295fa8f7a1c1
ives-xue <ivewxue@gmail.com>
2021-06-15 17:18:50 +0800

    ci: 项目工程初始化✔

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": [
+    "airbnb",
+    "prettier",
+    "prettier/vue"
+  ],
+  "singleQuote": true, // 使用单引号
+  "printWidth": 100, // 超过最大值换行
+  "trailingComma": "none",
+  "htmlWhitespaceSensitivity": "ignore",
+  "semi": false, // 结尾不用分号
+  "trailingComma": "none", // 函数最后不需要逗号
+  "tabWidth": 2                  //缩进
+}
